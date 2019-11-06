45fc80cec7f7607ae0984ce8e831692c9e5e2e77
du <du@192.168.10.1>
2019-11-06 15:37:24 +0800

    引入Redis

--- src/main/java/com/example/demo/config/DemoConfig.java
+++ src/main/java/com/example/demo/config/DemoConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,22 +11,25 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 @Configuration
 @MapperScan("com.example.demo.dao.*")
 public class DemoConfig {
+	@Bean
+	public HttpMessageConverters fastJsonHttpMessageConverters() {
+		// 1.定义一个converters转换消息的对象
+		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+		// 2.添加fastjson的配置信息，比如: 是否需要格式化返回的json数据
+		FastJsonConfig fastJsonConfig = new FastJsonConfig();
+		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+		// 3.在converter中添加配置信息
+		fastConverter.setFastJsonConfig(fastJsonConfig);
+		// 4.将converter赋值给HttpMessageConverter
+		HttpMessageConverter<?> converter = fastConverter;
+		// 5.返回HttpMessageConverters对象
+		return new HttpMessageConverters(converter);
+	}
 
-    @Bean
-    public HttpMessageConverters fastJsonHttpMessageConverters() {
-        // 1.定义一个converters转换消息的对象
-        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-        // 2.添加fastjson的配置信息，比如: 是否需要格式化返回的json数据
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-        // 3.在converter中添加配置信息
-        fastConverter.setFastJsonConfig(fastJsonConfig);
-        // 4.将converter赋值给HttpMessageConverter
-        HttpMessageConverter<?> converter = fastConverter;
-        // 5.返回HttpMessageConverters对象
-        return new HttpMessageConverters(converter);
-    }
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.alibaba.fastjson.JSON;
+import com.example.demo.common.RedisUtil;
+import com.example.demo.service.user.UserService;
+
+import redis.clients.jedis.JedisPool;
+
+
 
 @SpringBootTest
 class DemoApplicationTests {
-
+	@Autowired
+	UserService userService;
+	@Autowired
+	JedisPool jedisPool;
 	@Test
 	void contextLoads() {
+		RedisUtil.setV("user", userService.getUserInfo(), 0,jedisPool);
+		RedisUtil.delV("user",0, jedisPool);
+		System.out.println(JSON.toJSONString(RedisUtil.getVStr("user",0,jedisPool)));
 	}
 
 }
